e5842b4b26c0e63997e13f35a540058f4526fa53
Bikotoru <bikotoruvictor@gmail.com>
2024-02-17 17:56:02 -0500

    feat: no content
    Added class to change the placeholder color for when the user does not place data on the input

--- js/main.js
+++ js/main.js
@@ -1,26 +1,81 @@
+const resultado     = document.getElementById( "resultado"      );
+const submitButton  = document.getElementById( "submit-button"  );
 
+let activity;
+let age;
+let height;
+let weight;
 
+submitButton.addEventListener(
+    "click",
+    ()=>{
+        showResult();
+        
+        if( ! validate() ){
+            vanishResult();
 
-function calcularCalorias() {
-
-    const multiplicadorTMB = {
-        peso: 10,
-        altura: 6.25,
-        edad: 5
+            return;
+        }
     }
+);
+
+function validate(){
+    if( ! ( activity  = document.getElementById( "actividad"  ) ).value ){
+        activity.className = "form-control input-no-content";
+
+        showErrorMessage( "You have to place the intencity of your activity." );
+
+        return false;
+    }
+
+    if( ! ( age  = document.getElementById( "edad"  ) ).value ){
+        age.className = "form-control input-no-content";
+
+        showErrorMessage( "You have to place your age." );
+
+        return false;
+    }
+
+    if( ! ( height  = document.getElementById( "altura"  ) ).value ){
+        age.className = "form-control input-no-content";
+
+        showErrorMessage( "You have to place your height." );
+
+        return false;
+    }
+
+    if( ! ( weight  = document.getElementById( "peso"  ) ).value ){
+        age.className = "form-control input-no-content";
+
+        showErrorMessage( "You have to place your weight." );
+
+        return false;
+    }
+
+    return true;
+}
+
+function calcularCalorias(){
+    const multiplicadorTMB = {
+        peso    : 10,
+        altura  : 6.25,
+        edad    : 5
+    }
+
+    let calories;
 
         //Formula hombres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) + 5
 
         //Formula mujeres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) - 161
 
     
-    // totalCalorias.value = `${Math.floor(calculoCalorias)} kcal`;
+    totalCalories.value = `${ Math.floor( calories ) } kcal`;
     
     resultado.innerHTML = `
         <div class=" card-body d-flex flex-column justify-content-center align-items-center h-100" id="calculo">
             <h5 class="card-title h2">Calorías requeridas</h5>
             <div class="mb-3 w-100">
-                <input class="form-control text-center" value="${} kcal" style="font-size: 2rem" disabled>
+                <input class="form-control text-center" value="${ totalCalories } kcal" style="font-size: 2rem" disabled>
             </div>
         </div>
     `
@@ -28,51 +83,67 @@ function calcularCalorias() {
 
 }
 
-function mostrarMensajeDeError(msg) {
-    const calculo = document.querySelector('#calculo');
-    if (calculo) {
+function showErrorMessage( msg ){
+    const calculo = document.querySelector( '#calculo' );
+
+    if( calculo ){
         calculo.remove();
     }
 
     const divError = document.createElement('div');
+
     divError.className = 'd-flex justify-content-center align-items-center h-100';
-    divError.innerHTML = `<span class="alert alert-danger text-center">${msg}</span>`;
+    divError.innerHTML = `<span class="alert alert-danger text-center">${ msg }</span>`;
 
-    resultado.appendChild(divError);
+    resultado.appendChild( divError );
 
-    setTimeout(() => {
-        divError.remove();
-        desvanecerResultado();
-    }, 5000);
+    setTimeout(
+        () => {
+            divError.remove();
+
+            vanishResult();
+        },
+        5000
+    );
 }
 
 
-// Animaciones
-function aparecerResultado() {
-    resultado.style.top = '100vh';
+// Animations
+function showResult(){
+    resultado.style.top     = '100vh';
     resultado.style.display = 'block';
     
-    let distancia = 100;
-    let resta = 0.3;
-    let id = setInterval(() => {
-        resta *= 1.1;
-        resultado.style.top = `${distancia - resta}vh`;
-        if (resta > 100) {
-            clearInterval(id);
-        }
-    }, 10)
+    let distancia   = 100;
+    let resta       = 0.3;
+
+    let id  = setInterval(
+        () => {
+            resta               *= 1.1;
+            resultado.style.top = `${ distancia - resta }vh`;
+
+            if( resta > 100 ){
+                clearInterval( id );
+            }
+        },
+        10
+    )
 }
 
-function desvanecerResultado() {
+function vanishResult(){
     let distancia = 1;
 
-    let id = setInterval(() => {
-        distancia *= 2;
-        resultado.style.top = `${distancia}vh`;
-        if (distancia > 100) {
-            clearInterval(id);
-            resultado.style.display = 'none';
-            resultado.style.top = 0;
-        }
-    }, 10)
+    let id = setInterval(
+        () => {
+            distancia           *= 2;
+            resultado.style.top = `${ distancia }vh`;
+
+            if( distancia > 100 ){
+                clearInterval( id );
+
+                resultado.style.display = 'none';
+                resultado.style.top     = 0;
+            }
+        },
+        10
+    )
 }
